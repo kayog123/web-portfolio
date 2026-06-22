e26327234fec2c4d322ed48bb5bc2aaf25329039
Jovanni Jungco <jovannijungco27@gmail.com>
2026-06-23 03:53:34 +0800

    add adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-2379289103626273" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
